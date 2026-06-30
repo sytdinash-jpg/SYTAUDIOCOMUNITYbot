de026ce493ad3290eb103a01f8ec649495d7cc40
sytdinash-jpg <sytdinash@gmail.com>
2026-06-30 13:28:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client, filters
 
-# ரகசிய எண்கள் மற்றும் டோக்கனை Render செட்டிங்ஸ்ல இருந்து பாதுகாப்பா எடுக்கும் முறை
+# ரகசிய எண்கள்
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 bot_token = os.environ.get("BOT_TOKEN")
@@ -10,15 +10,17 @@ app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("பாட் தயார்! ஆியோவை அனுப்புங்க.")
+    await message.reply_text("பாட் தயார்! ஆடியோவை அனுப்புங்க.")
 
 @app.on_message(filters.audio)
 async def protect_audio(client, message):
-    # நீங்கள் கேட்ட பாதுகாப்பு வசதி (protect_content=True)
     await client.send_audio(
         chat_id=message.chat.id,
         audio=message.audio.file_id,
         protect_content=True
     )
+
+# வெப் சர்வீஸ் எரர் வராமல் தடுக்க இந்த ஒரு வரி மிக முக்கியம்!
+os.system(f"python3 -m http.server {os.environ.get('PORT', '8080')}")
 
 app.run()
