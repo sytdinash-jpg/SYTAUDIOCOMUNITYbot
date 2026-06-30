8a6c687a2c661166a95a7fb69ca9bc5a0fab3535
sytdinash-jpg <sytdinash@gmail.com>
2026-06-30 13:10:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+import os
 from pyrogram import Client, filters
 
-# இங்க உங்களோட API ID, HASH, TOKEN ஆகியவற்றை சரியாக மாற்றுங்கள்
-api_id = 36166001
-api_hash = "3fa97b32387a32b43a4074122b32de48"
-bot_token = "8974319677:AAG8hsnNN0HjiykXoYIgC7p9ijLPcz2jPy0"
+# ரகசிய எண்கள் மற்றும் டோக்கனை Render செட்டிங்ஸ்ல இருந்து பாதுகாப்பா எடுக்கும் முறை
+api_id = int(os.environ.get("API_ID"))
+api_hash = os.environ.get("API_HASH")
+bot_token = os.environ.get("BOT_TOKEN")
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text("பாட் தயார்! ஆடியோவை அனுப்புங்க.")
+    await message.reply_text("பாட் தயார்! ஆியோவை அனுப்புங்க.")
 
 @app.on_message(filters.audio)
 async def protect_audio(client, message):
+    # நீங்கள் கேட்ட பாதுகாப்பு வசதி (protect_content=True)
     await client.send_audio(
         chat_id=message.chat.id,
         audio=message.audio.file_id,
